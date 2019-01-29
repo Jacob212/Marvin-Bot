61c7b42908993c7ca4d553f70080f69fd21e7c08
Jacob Hale <jacobhale.hale@gmail.com>
2019-01-29 00:30:29 +0000

    Changed command check from role owner to my discord ID

--- Down.py
+++ Down.py
@@ -11,34 +11,37 @@ f.close()
 
 client = commands.Bot(command_prefix="?")
 
+def is_me(context):
+  return context.message.author.id == "130470072190894082"
+
 #Sets the played game status and prints that bot is logged in when ready.
 @client.event
 async def on_ready():
-	print('------')
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
-	await client.change_presence(game=Game(name="Down for maintenance"))
+  print('------')
+  print('Logged in as')
+  print(client.user.name)
+  print(client.user.id)
+  print('------')
+  await client.change_presence(game=Game(name="Down for maintenance"))
 
 @client.event
 async def on_command_error(error,context):
-	await client.send_message(context.message.channel,context.message.author.mention+" Sorry, The bot is down for maintenance")
+  await client.send_message(context.message.channel,context.message.author.mention+" Sorry, The bot is down for maintenance")
 
 #Changes the bot to the live version.
-@commands.has_role("Owner")
+@commands.check(is_me)
 @client.command(hidden=True,pass_context=True, aliases=["Start"])
 async def start(context):
-	await client.close()
-	print("Changing to live version")
-	os.system('python Marvin.py')
+  await client.close()
+  print("Changing to live version")
+  os.system('python Marvin.py')
 
 #Terminates the bot only if they have the role Owner.
-@commands.has_role("Owner")
+@commands.check(is_me)
 @client.command(hidden=True,pass_context=True, aliases=["Shutdown"])
 async def shutdown(context):
-	await client.say("Bye.")
-	await client.close()
+  await client.say("Bye.")
+  await client.close()
 
 
 client.run(TOKEN)
