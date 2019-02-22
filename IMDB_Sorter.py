c9bbf3b30b115e42af2cf508717a7834f520b91c
Jacob Hale <jacobhale.hale@gmail.com>
2019-02-22 17:38:17 +0000

    Improved processing time and cleaned it up abit

--- IMDB_Sorter.py
+++ IMDB_Sorter.py
@@ -16,48 +16,225 @@ def get_time(give_time):
   elapsed_time = time.time() - give_time
   minutes, seconds = divmod(elapsed_time,60)
   hours, minutes = divmod(minutes,60)
-  return f'{int(hours)}:{int(minutes)}:{seconds:.2f}'
+  return str(int(hours))+":"+str(int(minutes))+":"+str(round(seconds,2)) 
+
+def setup():
+  if os.path.isfile('./allMovies.tsv') and os.path.isfile('./allTV.tsv'):
+    menu()
+  else:
+    print("Sorting movies and tv series")
+    start_time = time.time()
+    basics = open("./basics/data.tsv", mode="r", encoding="utf-8")
+    movie = []
+    tv = []
+    for rownum, line in enumerate(basics):
+      line = line.rstrip("\n")
+      words = line.split("\t")
+      if words[1] == "movie":
+        movie.append(line)
+      elif words[1] == "tvSeries":
+        tv.append(line)
+    basics.close()
+    print("Finished in "+get_time(start_time))
+
+    print("summing languages")
+    start_time = time.time()
+    akas = open("./akas/data.tsv", mode="r", encoding="utf-8")
+    sumLanguages = []
+    total = []
+    languages = []
+    countries = []
+    for rownum, line in enumerate(akas):
+      line = line.rstrip("\n")
+      words = line.split("\t")
+      if words[0] != "titleId":
+        if total == []:
+          total = [words[0]]
+          languages = [words[4]]
+          countries = [words[3]]
+        else:
+          if words[0] == total[0]:
+            languages.append(words[4])
+            countries.append(words[3])
+          else:
+            sumLanguages.append(total[0]+"\t"+",".join(countries)+"\t"+",".join(languages))
+            total = [words[0]]
+            languages = [words[4]]
+            countries = [words[3]]
+    sumLanguages.append(total[0]+"\t"+",".join(countries)+"\t"+",".join(languages))
+    akas.close()
+    print("Finished in "+get_time(start_time))
+
+    print("adding languages to movies")
+    start_time = time.time()
+    short = open("./allMovies.tsv", mode="w", encoding="utf-8")
+    index = 0
+    for line in movie:
+      line = line.rstrip("\n")
+      words = line.split("\t")
+      done = 0
+      while index < len(sumLanguages):
+        line2 = sumLanguages[index].rstrip("\n")
+        index += 1
+        words2 = line2.split("\t")
+        if words[0] == words2[0]:
+          short.write(line+"\t"+words2[1]+"\t"+words2[2]+"\tNone\tNone\n")
+          done = 1
+          break
+        if words2[0] > words[0]:
+          index -= 1
+          break
+      if done == 0:
+        short.write(line+"\tNone\tNone\tNone\tNone\n")
+    short.close()
+    print("Finished in "+get_time(start_time))
+
+    print("adding languages to tv")
+    start_time = time.time()
+    tvLanguages = []
+    index = 0
+    for line in tv:
+      line = line.rstrip("\n")
+      words = line.split("\t")
+      done = 0
+      while index < len(sumLanguages):
+        line2 = sumLanguages[index].rstrip("\n")
+        index += 1
+        words2 = line2.split("\t")
+        if words[0] == words2[0]:
+          tvLanguages.append(line+"\t"+words2[1]+"\t"+words2[2]+"\n")
+          done = 1
+          break
+        if words2[0] > words[0]:
+          index -= 1
+          break
+      if done == 0:
+        tvLanguages.append(line+"\tNone\tNone\n")
+    print("Finished in "+get_time(start_time))
+
+    print("Splitting lines with season and episode numbers up from the ones without.")
+    start_time = time.time()
+    episodes = open("./episodes/data.tsv", mode="r", encoding="utf-8")
+    info = []
+    noInfo = []
+    for rownum, line in enumerate(episodes):
+      line = line.rstrip("\n")
+      words = line.split("\t")
+      if not(words[2] == "\\N" or words[3] == "\\N" or words[0] == "tconst"):
+        info.append(words)
+      elif not(words[0] == "tconst"):
+        noInfo.append(words)
+    episodes.close()
+    info = sorted(info,key=lambda x:(x[1],int(x[2]),int(x[3])))
+    print("Finished in "+get_time(start_time))
+
+    print("Adding up total episodes in each season.")
+    start_time = time.time()
+    numbers = []
+    total = []
+    for line in info:
+      if total == []:
+        total = [line[1]+"\t"+line[2]]
+      else:
+        if line[1]+"\t"+line[2] in total:
+          total.append(line[1]+"\t"+line[2])
+        else:
+          numbers.append(total[0]+"\t"+str(len(total)))
+          total = [line[1]+"\t"+line[2]]
+    numbers.append(total[0]+"\t"+str(len(total)))
+    print("Finished in "+get_time(start_time))
+
+    print("Adding season and episode numbers to all the other info.")
+    start_time = time.time()
+    short = open("./allTV.tsv", mode="w", encoding="utf-8")
+    index = 0
+    for line in tvLanguages:
+      line = line.rstrip("\n")
+      words = line.split("\t")
+      done = 0
+      while index < len(numbers):
+        words2 = numbers[index].split("\t")
+        index += 1
+        if words[0] == words2[0]:
+          short.write(line+"\t"+words2[1]+"\t"+words2[2]+"\n")
+          done = 1
+        if words2[0] > words[0]:
+          index -= 1
+          break
+      if done == 0:
+        short.write(line+"\tNone\tNone\n")
+    short.close()
+    print("Finished in "+get_time(start_time))
+    menu()
 
 def menu():
   while True:
-    print("======================================================")
-    print("1: Movie file")
-    print("2: TV file")
-    print("3: List all genres")
-    print("4: List all languages")
-    print("5: List all regions")
-    print("6: Import all Movie files")
-    print("7: Import all TV files")
-    print("8: Exit")
+    print("="*50)
+    print("1: Search movies")
+    print("2: Search tv")
+    print("3: List all years")
+    print("4: List all genres")
+    print("5: List all languages")
+    print("6: List all countries")
+    print("7: Exit")
     option = input("Enter one of the options above: ")
     if option == "1" or option == "2":
-      year = input("Enter the year you want: ")
-      language = input("Enter the language you want: ")
-      region = input("Enter the region you want: ")
-      print("======================================================\n")
+      print("="*50)
+      year = input("Enter a year(Leave blank if none): ")
+      genre = input("Enter a genre(Leave blank if none): ")
+      language = input("Enter a language(Leave blank if none): ").lower()
+      country = input("Enter a country(Leave blank if none): ").lower()
       if option == "1":
-        movie_file(year,language,region)
-      else:
-        tv_file(year)
+        searchFile(year,language,country,genre,"allMovies")
+      elif option == "2":
+        searchFile(year,language,country,genre,"allTV")
     elif option == "3":
-      print("======================================================\n")
-      genres()
+      years()
     elif option == "4":
-      print("======================================================\n")
-      languages()
+      genres()
     elif option == "5":
-      print("======================================================\n")
-      regions()
+      languages()
     elif option == "6":
-      print("======================================================\n")
-      importMovies()
+      countries()
     elif option == "7":
-      print("======================================================\n")
-      importTV()
-    elif option == "8":
       break
     else:
-      print("plz pick again")
+      print("Invalid option. please try again")
+
+def searchFile(year,language,country,genre,file):
+  print("Searching file for given options")
+  start_time = time.time()
+  inFile = open("./"+file+".tsv", mode="r", encoding="utf-8")
+  outFile = open("./"+file+year+language+country+".tsv", mode="w", encoding="utf-8")
+  for rownum, line in enumerate(inFile):
+    words = line.split("\t")
+    if re.search(year,words[5]) and re.search(country,words[9].lower()) and re.search(language,words[10]) and re.search(genre,words[8].lower()):
+      outFile.write(line)
+  outFile.close()
+  inFile.close()
+  print("Finished in "+get_time(start_time))
+
+def years():
+  start_time = time.time()
+  basics = open('./basics/data.tsv', mode='r', encoding='utf-8')
+  years = open('./YEARS.tsv', mode='w', encoding='utf-8')
+  totalYears = []
+  yearsNone = None
+  for rownum, line in enumerate(basics):
+    line = line.rstrip("\n")
+    words = line.split("\t")
+    if not(words[5] in totalYears) and words[5] != "startYear" and words[5] != "\\N":
+      totalYears.append(words[5])
+    elif words[5] == "\\N":
+      yearsNone = words[5]
+  for year in sorted(totalYears, key=lambda x:int(x),reverse=True):
+    years.write(year+"\n")
+    print(year)
+  years.write(yearsNone+"\n")
+  print(yearsNone)
+  years.close()
+  basics.close()
+  print("\nFinished in "+get_time(start_time)+"\n")
 
 def genres():
   start_time = time.time()
@@ -69,11 +246,9 @@ def genres():
     words = line.split("\t")
     genresList = words[8].split(",")
     for genre in genresList:
-      if genre == "genres":
-        pass
-      elif genre in totalGenres:
+      if genre in totalGenres and genre != "genres":
         totalGenres[genre] += 1
-      else:
+      elif genre != "genres":
         totalGenres[genre] = 1
   for k, v in sorted(totalGenres.items(), key = itemgetter(1),reverse=True):
     genres.write(k+"\t"+str(v)+"\n")
@@ -105,196 +280,29 @@ def languages():
   languages.close()
   print("\nFinished in "+get_time(start_time)+"\n")
 
-def regions():
+def countries():
   start_time = time.time()
   akas = open('./akas/data.tsv', mode='r', encoding='utf-8')
-  regions = open('./REGIONS.tsv', mode='w', encoding='utf-8')
-  totalRegions = {}
+  countries = open('./REGIONS.tsv', mode='w', encoding='utf-8')
+  totalCountries = {}
   for rownum, line in enumerate(akas):
     line = line.rstrip("\n")
     words = line.split("\t")
-    regionsList = words[3].split(",")
-    for region in regionsList:
-      if region == "region":
+    countriesList = words[3].split(",")
+    for country in countriesList:
+      if country == "region":
         pass
-      elif region in totalRegions:
-        totalRegions[region] += 1
+      elif country in totalCountries:
+        totalCountries[country] += 1
       else:
-        totalRegions[region] = 1
-  for k, v in sorted(totalRegions.items(), key = itemgetter(1),reverse=True):
-    regions.write(k+"\t"+str(v)+"\n")
+        totalCountries[country] = 1
+  for k, v in sorted(totalCountries.items(), key = itemgetter(1),reverse=True):
+    countries.write(k+"\t"+str(v)+"\n")
     print(k+": "+str(v))
   akas.close()
-  regions.close()
+  countries.close()
   print("\nFinished in "+get_time(start_time)+"\n")
 
-def sortByRegionAndLanguage(language,region):
-  start_time = time.time()
-  akas = open('./akas/data.tsv', mode='r', encoding='utf-8')
-  sortedList = []
-  for rownum, line in enumerate(akas):
-    line = line.rstrip("\n")
-    words = line.split("\t")
-    if words[3] == region and words[4] == language:
-      sortedList.append(line)
-    elif words[4] == language and region == "":
-      sortedList.append(line)
-    elif words[3] == region and language == "":
-      sortedList.append(line)
-  akas.close()
-  print(len(sortedList))
-  print("Sorting by language and/or region finished in "+get_time(start_time))
-  return sortedList
-
-def movie_file(year,language,region):
-  start_time = time.time()
-  basics = open('./basics/data.tsv', mode='r', encoding='utf-8')
-  movies = []
-  for rownum, line in enumerate(basics):
-    words = line.split("\t")
-    if words[1] == "movie":
-      movies.append(line)
-  basics.close()
-  partTwoTime = time.time()
-  movie = open('./MOVIE'+year+language+region+'.tsv', mode='w', encoding='utf-8')
-  regionAndLanguageList = sortByRegionAndLanguage(language,region)
-  count = 0
-  for line in movies:
-    count += 1
-    line = line.rstrip("\n")
-    words = line.split("\t")
-    if (time.time()-partTwoTime) > 10:
-      print("Number of movies checked: "+str(count)+" Total time: "+get_time(start_time))
-      partTwoTime = time.time()
-    formatLine = "MovieID\t"+words[1]+"\t"+words[2]+"\t"+words[3]+"\t"+"none"+"\t"+"none"+"\t"+words[5]+"\t"+words[7]+"\t"+words[8]+"\t"+words[0]+"\n"
-    if language != "" or region != "":
-      for line2 in regionAndLanguageList:
-        words2 = line2.split("\t")
-        if words[5] == year and words[0] == words2[0]:
-          movie.write(formatLine)
-          regionAndLanguageList.remove(line2)
-          break
-    elif words[5] == year:
-      movie.write(formatLine)
-    elif year == "":
-      movie.write(formatLine)
-  print(count)
-  movie.close()
-  print("Finished in "+get_time(start_time))
-
-def tv_file(year="ALL"):
-  run_time = time.time()
-  start_time = time.time()
-  basics = open('./basics/data.tsv', mode='r', encoding='utf-8')
-  tv = []
-  for rownum, line in enumerate(basics):
-    line = line.rstrip("\n")
-    words = line.split("\t")
-    if year == "":
-      if words[1] == "tvSeries":
-        tv.append(line)
-    else:
-      if words[1] == "tvSeries" and words[5] == year:
-        tv.append(line)
-  basics.close()
-  print("step 1 finished in "+get_time(start_time)+" total time: "+get_time(run_time))
-
-  
-  start_time = time.time()
-  episodes = open('./episodes/data.tsv', mode='r', encoding='utf-8')
-  episodes2 = []
-  for rownum, line in enumerate(episodes):
-    line = line.rstrip("\n")
-    words = line.split("\t")
-    if not(words[2] == "\\N" or words[3] == "\\N" or words[0] == "tconst"):
-      episodes2.append(words)
-  episodes.close()
-  print("step 2 finished in "+get_time(start_time)+" total time: "+get_time(run_time))
-  start_time = time.time()
-  sortfile = []
-  sort = sorted(episodes2,key=lambda x:(x[1],int(x[2]),int(x[3])))
-  for eachline in sort:
-    line = "\t".join(eachline)
-    sortfile.append(line)
-  print("step 3 finished in "+get_time(start_time)+" total time: "+get_time(run_time))
-  start_time = time.time()
-  numbers = []
-  total = []
-  for line in sortfile:
-    words = line.split("\t")
-    if total == []:
-      total = [words[1]+"\t"+words[2]]
-    else:
-      if words[1]+"\t"+words[2] in total:
-        total.append(words[1]+"\t"+words[2])
-      else:
-        numbers.append(total[0]+"\t"+str(len(total)))
-        total = [words[1]+"\t"+words[2]]
-  print("step 4 finished in "+get_time(start_time)+" total time: "+get_time(run_time))
-  start_time = time.time()
-  partTwoTime = time.time()
-  tvnumbers = open('./TV'+year+'.tsv', mode='w', encoding='utf-8')
-  count = 0
-  for line in tv:
-    count += 1
-    line_time = time.time()
-    words = line.split("\t")
-    for line2 in numbers:
-      words2 = line2.split("\t")
-      if words[0] == words2[0]:
-        tvnumbers.write(line+"\t"+words2[1]+"\t"+words2[2]+"\n")
-        numbers.remove(line2)
-    if (time.time()-partTwoTime) > 10:
-      print("Number of tv shows checked: "+str(count)+" Total time: "+get_time(start_time))
-      partTwoTime = time.time()
-  tvnumbers.close()
-  print("step 5 finished in "+get_time(start_time)+" total time: "+get_time(run_time))
-
-def importMovies():
-  movies = open('./MOVIE.tsv', mode='r', encoding='utf-8')
-  for rownum, line in enumerate(movies):
-    words = line.split("\t")
-    final = []
-    for word in words:
-      print(word)
-      if word == "\\N":
-        final.append(None)
-      else:
-        final.append(word)
-    primaryTitle = " ".join(re.findall('\w+',final[2]))
-    originalTitle = " ".join(re.findall('\w+',final[3]))
-    genres = None
-    if final[8] != None:
-      genres = "\t".join(re.findall('[\w-]+',final[8]))
-    c.execute('''SELECT Movies.primaryTitle FROM Movies WHERE primaryTitle = "'''+primaryTitle+'''" ORDER BY Movies.primaryTitle, Movies.Season;''')
-    conn.commit()
-    result = c.fetchall()
-    if result == []:
-      c.execute('''INSERT INTO movies VALUES(?,?,?,?,?,?,?,?,?,?)''', (None,final[1],primaryTitle,originalTitle,None,None,final[6],final[7],genres,final[9]))
-      conn.commit()
-  movies.close()
-
-def importTV():
-  tv = open('./TV.tsv', mode='r', encoding='utf-8')
-  for rownum, line in enumerate(tv):
-    words = line.split("\t")
-    title = " ".join(re.findall('\w+',words[2]))
-    genres = "\t".join(re.findall('[\w-]+',words[8]))
-    if genres == "N":
-      genres = None
-    c.execute('''SELECT Movies.Title FROM Movies WHERE Title = "'''+title+'''" ORDER BY Movies.Title, Movies.Season;''')
-    conn.commit()
-    result = c.fetchall()
-    if result == []:
-      c.execute('''INSERT INTO movies VALUES(?,?,?,?,?,?)''', (None,title,None,None,None,genres))
-      conn.commit()
-  tv.close()
-
-
-
-
-
-
-menu()
+setup()
 c.close()
 conn.close()
